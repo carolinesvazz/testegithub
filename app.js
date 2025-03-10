66aa3537e6b12ec35d0f971a8cca9b5117889ba8
Caroline Vaz <carolinesvazz@gmail.com>
2025-03-10 07:13:19 -0300

    alterando as informações

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-let numeroInteiroAleatorio = parseInt(Math.random() * 100) +1;
-console.log(numeroInteiroAleatorio);
+//Como conectar Github local com remoto
+ //Digite no terminal
+
+git init
+git add .
+git commit -m "projeto inicial"
+git remote add origin
+git remote add origin git@githucarolinesvazz/testegithub.git
+git push -u origin main(master)
 
 //Realizando um commit e subindo no Git
 
-let jogodonumerosecreto = 10;
+git add .
+git commit -m "alterando o limite do jogo"
+git log 
+
+//Conectando o repositório local com o remoto
+
+git remote origin
+git push origin master
